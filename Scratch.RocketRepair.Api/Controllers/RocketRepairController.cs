36059703aae00e759cda54435ba68ed83cf8012b
Jace O'Brien <servicefabricdev@gmail.com>
2022-01-15 17:23:00 -0600

    Init

---
+++ Scratch.RocketRepair.Api/Controllers/RocketRepairController.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Mime;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Scratch.RocketRepair.Api.Controllers
+{
+    [ApiController]
+    [Route("rocketrepair")]
+    public class RocketRepairController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<RocketRepairController> _logger;
+        private readonly SecretClient _secretClient;
+
+        public RocketRepairController(ILogger<RocketRepairController> logger, SecretClient secretClient)
+        {
+            _logger = logger;
+            _secretClient = secretClient;
+        }
+
+        [HttpGet]
+        [RequestSizeLimit(75000000)]
+        public IActionResult Get()
+        {
+            // Name of the share, directory, and file we'll download from
+            const string shareName = "scratch-rocketrepair";
+            const string dirName = "release/0.0.1";
+            const string fileName = "RocketRepair.zip";
+            const string contentType = MediaTypeNames.Application.Zip;
+
+            var connectionString = _secretClient.GetSecret("connectionstring-storageaccount");
+
+            // Path to the save the downloaded file
+            // Get a reference to the file
+            var share = new ShareClient(connectionString.Value.Value, shareName);
+            var directory = share.GetDirectoryClient(dirName);
+            var file = directory.GetFileClient(fileName);
+
+            // Download the file
+            var download = (ShareFileDownloadInfo) file.Download();
+            return File(download.Content, contentType, fileName);
+        }
+    }
+}
---
+++ Scratch.RocketRepair.Api/Startup.cs
@@ -0,0 +1,60 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Scratch.RocketRepair.Api
+{
+    public class ConnectionStrings
+    {
+        public string KeyVault { get; set; }
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var client = new SecretClient(new(Configuration.GetConnectionString("KeyVault")), new DefaultAzureCredential());
+            services.AddSingleton(client);
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new()
+                    {Title = "Scratch.RocketRepair.Api", Version = "v1"});
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Scratch.RocketRepair.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
